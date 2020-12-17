8f0c22ef2bb03fb3802e6150c8e3c85432010aa8
linkzepp <adriftchen@gmail.com>
2020-12-17 08:17:00 +0800

    編輯畫面顯示修正

--- app2/api/query.php
+++ app2/api/query.php
@@ -4,13 +4,13 @@ include_once "../base.php";
 $rows=$Stu->all();
 
 foreach($rows as $row){
-echo "<td>";
+echo "<tr>";
 echo "<td>{$row['id']}</td>";
 echo "<td>{$row['name']}</td>";
 echo "<td>{$row['classes']}</td>";
 echo "<td>{$row['num']}</td>";
 echo "<td>{$row['score']}</td>";
-echo " </td>";
+echo "<td>";
 echo "<button type='button' data-id='{$row['id']}' onclick='del({$row['id']})'>刪除</button>";/* data-id可以用數字 寫允許重複 */
 echo "<button type='button' data-id='{$row['id']}' onclick='op({$row['id']})'>編輯</button>";
 echo "</td>";
